1438b02111d3d8b261229f95581bc011a1005122
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-11-26 17:01:11 -0800

    Update address_parse_tb.sv
    adding debug and changing sizes to make testing faster.

--- address_parse_tb.sv
+++ address_parse_tb.sv
@@ -1,13 +1,19 @@
 module address_parse_tb;
 
 //parameters for memory size and organization (in bits used to represent them)
-parameter integer i_size = 64;
-parameter integer c_size = 14;
+//instruction size
+parameter integer i_size = 16;
+//capacity size
+parameter integer c_size = 10;
+//associativity
 parameter integer a_size = 3;
+//data size
 parameter integer d_size = 6;
 
 //for looping
 integer i;
+
+integer debug = 0;
 
 
 //define address and individual section sizes
@@ -21,21 +27,24 @@ address_parse #(.instruction_size(i_size), .capacity(c_size), .data_lines(d_size
 
 initial
 begin
+	if($test$plusargs ("debug"))
+		debug = 1;
 	//loop through all possible values
-	for(i = 0; i < 2 ** (c_size - a_size); i = i + 1)
+	for(i = 0; i < 2 ** (i_size); i = i + 1)
 	begin
 		//assign value to address
 		address = i;
 		//check that the values we get back from the module are correct
-		if(byte_select !== address[d_size - 1 : 0] || index !== address[(c_size - a_size) + d_size - 1 : d_size] || tag !== address[i_size - (c_size - a_size) - d_size + d_size + (c_size - a_size) - 1 : d_size + (c_size - a_size)])
+		if((byte_select !== address[d_size - 1 : 0] || index !== address[(c_size - a_size) + d_size - 1 : d_size] || tag !== address[i_size - (c_size - a_size) - d_size + d_size + (c_size - a_size) - 1 : d_size + (c_size - a_size)]) || debug)
 		begin
 			//if not, display the error information
-			$display("\nincorrect");
+			$display("\n\nincorrect:");
 			$displayh("input: ", address, "\nbyte_select: ", byte_select, "\nindex: ", index, "\ntag: ", tag);
 			$displayh("\nShould be: \nbyte_select:", address[d_size - 1 : 0], "\nindex: ", address[(c_size - a_size) + d_size - 1 : d_size], "\ntag: ", address[i_size - (c_size - a_size) - d_size + d_size + (c_size - a_size) - 1 : d_size + (c_size - a_size)]); 
 		end
+
 	end
+	$display("Finished.");
 end
 
 endmodule
-		
