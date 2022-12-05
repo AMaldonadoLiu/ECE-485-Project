9e7ccd829087088408b2bc4c69143c40a8b9e6b1
wil822 <115514364+wil822@users.noreply.github.com>
2022-12-05 15:00:55 -0800

    Updated with commits. Can someone explain line 43?

--- update_LRU.sv
+++ update_LRU.sv
@@ -2,10 +2,10 @@ module update_LRU(block_select, LRU_bits, returned);
 parameter associativity = 8;
 
 
-output reg [associativity - 2 : 0]returned;
+output reg [associativity - 2 : 0]returned; //size of the output (returned value)
 
-input [associativity - 2 : 0]LRU_bits;
-input [$clog2(associativity) - 1 : 0]block_select;
+input [associativity - 2 : 0]LRU_bits; // size of LRU bits
+input [$clog2(associativity) - 1 : 0]block_select; //size of each block (based on log_2 calculation of associativity, log_2(8))
 
 int i = 0;
 int a = 0;
@@ -27,25 +27,25 @@ begin
 
 	for(i = 0; i < associativity - 1; i = i + 1)
 	begin
-		if(i == a)
+		if(i == a) // if true
 		begin
-			//$display("I made it here.");
-			returned[i] = block_select[$clog2(associativity) - 1 - b];
-			if(block_select[$clog2(associativity) - 1 - b]%2 == 0)
+			//$display("I made it here."); //the value made it to it's specific block location
+			returned[i] = block_select[$clog2(associativity) - 1 - b]; //output the value from that same block location
+			if(block_select[$clog2(associativity) - 1 - b]%2 == 0) //if the selected block is even
 			begin
-				a = 2 * a + 1;
+				a = 2 * a + 1; //go left
 			end
 		
-			else
+			else //if the selected block is odd
 			begin
-				a = 2 * a + 2;
+				a = 2 * a + 2; // go right
 			end
-			b = b + 1;
+			b = b + 1; //increment (b) for so it can also go to the next block value (line 33)?
 		end
 			
-		else
+		else //if the value is not true (line 30), 
 		begin
-			returned[i] = LRU_bits[i];
+			returned[i] = LRU_bits[i]; //output the value from that specific LRU bit, which will be a temp value
 			//$display("using temp");
 		end
 		//$display(returned, " ", i);
@@ -66,7 +66,3 @@ end
 
 
 endmodule
-
-
-
- 
