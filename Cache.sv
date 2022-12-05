fa497a00f6f0015fff0ec9311f1ad0134dd1dbfe
Bernadeau Faurestal <72511661+kolabohub@users.noreply.github.com>
2022-12-05 12:17:55 -0800

    added some comments

--- Cache.sv
+++ Cache.sv
@@ -1,16 +1,21 @@
 module cache(command, instruction, tag_array, hit, bus_op_in, bus_op_out, common, snoop_result, L2_L1);
+/*This module takes in the command, tag_array, and output 
+**the bus operation
+**the snoop_result
+**and L2_L1
 
+*/
 //this uses powers of 2
-parameter integer i_size = 32;
+parameter integer i_size = 32; 
 parameter integer c_size = 24;
 parameter integer d_size = 6;
-parameter integer protocol = 2;
+parameter integer protocol = 2; 
 
 //this one is in bits
 parameter integer a_size = 8;
 parameter integer max_array = a_size + (protocol + i_size - c_size + $logc2(a_size) - d_size) * a_size - 2;
 
-input integer command;
+input integer command; // command from trace file 
 input [i_size - 1 : 0] instruction;
 input [1:0] bus_op_in;
 
@@ -18,19 +23,19 @@ inout [a_size + (protocol + i_size - c_size + a_size - d_size) * a_size - 2: 0] 
 inout common;
 
 output hit;
-output [1:0] bus_op_out;
-output [1:0] snoop_result;
-output [1:0] L2_L1;
+output [1:0] bus_op_out; //bus operation commands 
+output [1:0] snoop_result; //update snoop_result 
+output [1:0] L2_L1; // ??
 
 
 
 integer i;
 
-wire [i_size - c_size + $clog2(a_size) - c_size - d_size - 1 :0] tag;
-wire [(c_size - $clog2(a_size)) - 1 : 0] index;
-wire [d_size - 1 : 0] byte_select;
-reg [$clog2(a_size) - 1 : 0] block_select;
-reg [max_array : max_array - a_size + 1] returned;
+wire [i_size - c_size + $clog2(a_size) - c_size - d_size - 1 :0] tag; //tag bits
+wire [(c_size - $clog2(a_size)) - 1 : 0] index; // num of index bits
+wire [d_size - 1 : 0] byte_select; //num of byte_select bits
+reg [$clog2(a_size) - 1 : 0] block_select; // num of block_select??
+reg [max_array : max_array - a_size + 1] returned; // ??
 
 
 address_parse #(.instruction_size(i_size), .data_lines(d_size), .capacity(c_size), .associativity(a_size)) a_parse (instruction, tag, index, byte_select);
--- hit_miss.sv
+++ hit_miss.sv
@@ -1,15 +1,16 @@
 module hit_miss(tag, /*index,*/ tag_array, MESI, hit, miss);
 
 //this uses powers of 2
-parameter integer i_size = 32;
-parameter integer c_size = 24;
-parameter integer d_size = 6;
-parameter integer protocol = 2;
+parameter integer i_size = 32; //instruction size
+parameter integer c_size = 24; // cache capacity
+parameter integer d_size = 6; //number of bit for byte_select
+parameter integer protocol = 2; // ??
 
 //this one is in bits
 parameter integer a_size = 8;
-
-input [i_size - c_size + a_size - d_size - 1 : 0]tag;
+/* substracting the c_size + the a_size and the d_size from i_size to get
+the bits for the tag_array*/
+input [i_size - c_size + a_size - d_size - 1 : 0]tag; //in our case tag_array should be 15 bits
 //input [(c_size - a_size) - 1 : 0]index;
 input [i_size - c_size + a_size - d_size - 1: 0] tag_array[a_size];
 input [protocol-1 : 0] MESI[a_size];
@@ -22,7 +23,7 @@ begin
 	//$display("Here I am.");
 	for(int i = 0; i < a_size; i = i + 1)
 	begin 
-		if(tag_array[i] === tag && MESI[i] !== 0)
+		if(tag_array[i] === tag && MESI[i] !== 0) // check if we have tag in cache while checking we're not in invalidate state
 		begin
 			hit = 1;
 			miss = 0;
