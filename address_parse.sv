d6462286c97b81d93b8f9b416d6578ac5822f5c9
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-06 22:26:41 -0800

    Update address_parse.sv

--- address_parse.sv
+++ address_parse.sv
@@ -1,21 +1,21 @@
 module address_parse(address, tag, index, byte_select);
 //parameters to be changed depending on the architecture (in bits used to represent them)
-parameter integer instruction_size = 64;
-parameter integer data_lines = 6;
-parameter integer capacity = 14;
-parameter integer associativity = 3;
+parameter integer i_size = 64;
+parameter integer d_size = 6;
+parameter integer c_size = 14;
+parameter integer a_size = 8;
 
 //the defined size of an instruction
-input [instruction_size - 1: 0]address;
+input [i_size - 1: 0]address;
 
 //the size of the output section arrays
-output [data_lines - 1 : 0]byte_select;
-output [(c_size - $clog2(a_size)) - d_size - 1 : 0]index;
-output [instruction_size - data_lines - capacity + associativity - 1 : 0]tag;
+output [d_size - 1 : 0]byte_select;
+output [(c_size - $clog2(a_size) - d_size) - 1 : 0]index;
+output [i_size - (c_size - d_size - $clog2(a_size)) - d_size - 1 : 0]tag;
 
 //assign different sections of the data to each output
-assign byte_select = address[data_lines - 1 : 0];
-assign index = address[(capacity - associativity) + data_lines - 1 : data_lines];
-assign tag = address[instruction_size - 1 : (capacity - associativity) + data_lines];
+assign byte_select = address[d_size - 1 : 0];
+assign index = address[(c_size - $clog2(a_size)) + d_size - 1 : d_size];
+assign tag = address[i_size - 1 : (c_size - $clog2(a_size)) + d_size];
 
 endmodule
