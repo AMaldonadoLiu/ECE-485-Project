2ed393bb5bfe205c02c173a045da766c0e77dd82
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-06 20:02:35 -0800

    Update Cache.sv

--- Cache.sv
+++ Cache.sv
@@ -44,7 +44,7 @@ address_parse #(.instruction_size(i_size), .data_lines(d_size), .capacity(c_size
 block_selector  #(.i_size(i_size), .d_size(d_size), .c_size(c_size), .a_size(a_size), .protocol(protocol)) selector (tag_array[index], block_select);
 //update_LRU #(.a_size(a_size)) uL (block_select, tag_array[index][max_array : max_array - a_size + 1], returned);
 
-	reg [31:0] tag_array[c_size - $clog2(a_size) -1:0][a_size-1:0];
+	reg [31:0] tag_array[(c_size - $clog2(a_size)) - d_size - 1:0][a_size-1:0];
 //assign tag_array[index][max_array : max_array - a_size + 1] = returned; 
 //assign tag_array[index][(protocol + i_size - c_size + a_size - d_size) * (block_select + 1) - protocol : (protocol + i_size - c_size + a_size - d_size) * block_select] = 
 
