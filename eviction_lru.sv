147ba5b5fa890ff17218d006f754c297914e3d7e
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-06 20:23:17 -0800

    Update eviction_lru.sv

--- eviction_lru.sv
+++ eviction_lru.sv
@@ -1,12 +1,16 @@
 module eviction_lru(block_select, LRU_bits);
-parameter associativity = 8; //this is the number of ways for each index
+parameter associativity = 8;
 
 
 input [associativity - 2 : 0]LRU_bits; // size of LRU bits
 output reg [$clog2(associativity) - 1 : 0]block_select; //size of each block (based on log_2 calculation of associativity, log_2(8))
 
-int i = 0; //for assigning each bit of the block_select
-int a = 0; //for the index of the LRU_bits array
+int i = 0;
+int a = 0;
+int b = 0;
+
+int count = 0;
+
 
 
 
@@ -21,13 +25,15 @@ begin
 	begin
 
 		block_select[$clog2(associativity) - 1 - i] = LRU_bits[a];
-		if(LRU_bits[a] == 1) //if the selected block is even
+		if(LRU_bits[a] == 0) //if the selected block is even
 		begin
+			block_select[$clog2(associativity) - 1 - i] = 1;
 			a = 2 * a + 2; //go right
 		end
 	
 		else // selected block must be odd
 		begin
+			block_select[$clog2(associativity) - 1 - i] = 0;
 			a = 2 * a + 1; // go left
 		end
 			
