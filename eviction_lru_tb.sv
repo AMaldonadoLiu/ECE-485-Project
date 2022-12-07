a4acc9c0cbc993d2193fdbafbd93d4e3cec31909
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-06 17:09:03 -0800

    Update eviction_lru_tb.sv

--- eviction_lru_tb.sv
+++ eviction_lru_tb.sv
@@ -11,56 +11,52 @@ integer debug = 0;
 reg [associativity - 2 : 0] returned;
 reg [associativity - 2 : 0] LRU_bits;
 reg [$clog2(associativity) - 1 : 0] block_select;
-reg [associativity - 2 : 0] check = 0;
+reg [$clog2(associativity) - 1 : 0] check;
 reg [associativity - 2 : 0] temp = 0;
 
-eviction_lru #(.associativity(associativity)) test(block_select, LRU_bits, returned);
+int num_wrong = 0;
+
+eviction_lru #(.associativity(associativity)) test(block_select, LRU_bits);
 
 
 initial
 begin 
 	if($test$plusargs ("debug")) //checking if debug was called in the terminal
 		debug = 1;
-	for(i = 0; i < associativity; i = i + 1)
+		
+	for(j = 0; j < 2 ** (associativity-1); j = j + 1) //next branch of LRU tree
 	begin
-		block_select = i; // set initial branch (very top of LRU tree) to the value of i
-		for(j = 0; j < 2 ** (associativity-1); j = j + 1) //next branch of LRU tree
+		LRU_bits = j;
+		a = 0;
+		for(k = 0; k < $clog2(associativity); k = k + 1) //final branch of LRU tree (the actual block)
 		begin
-			LRU_bits = j;
-			check = j;
-			a = 0;
-			for(k = 0; k < $clog2(associativity); k = k + 1) //final branch of LRU tree (the actual block)
+			check[2-k] = LRU_bits[a]; //set the check[a] to the selected block value
+			if(LRU_bits[a] % 2 === 0) //if value is even
 			begin
-				if(a > associativity-2) //if (a) is greater than the associativity, go to line 50
-					break;
-				check[a] = block_select[$clog2(associativity) - 1 - k]; //set the check[a] to the selected block value
-				if(check[a] % 2 === 0) //if value is even
-				begin
-					a = 2 * a + 1; // go right
-				end
-
+				a = 2 * a + 1; // go left
+			end
 				else //value must be odd
-				begin
-					a = 2 * a + 2; //go left
-				end
+			begin
+				a = 2 * a + 2; //go right
 			end
-			//$display("Before delay: ", LRU_bits);
-			#0;
-			//$display("After delay: ", LRU_bits);
-			if(returned ===  check || debug) //if the output value equals either the check value or debug
-			begin // display the evicted value
-				//$displayb("\nblock select: ", block_select);
-				//$displayb("LRU bits: ", j);
-				//$displayb("should be: ", check);
-				$displayb("Evicted: ", returned);
-			end
-			else //(returned !==  check || debug) //the output value does not equal the check value or debug
-			begin // display and compare the results to each other
-				$displayb("\nblock select: ", block_select);
-				$displayb("LRU bits: ", j);
-				$displayb("should be: ", check);
-				$displayb("Returned: ", returned);
-			end
+		end
+		//$display("Before delay: ", LRU_bits);
+		#1
+		//$display("After delay: ", LRU_bits);
+		if(block_select ===  check || debug) //if the output value equals either the check value or debug
+		begin // display the evicted value
+			//$displayb("\nblock select: ", block_select);
+			//$displayb("LRU bits: ", j);
+			//$displayb("should be: ", check);
+			$display("Evicted: ", block_select);
+		end
+		else //(returned !==  check || debug) //the output value does not equal the check value or debug
+		begin // display and compare the results to each other
+			num_wrong++;
+			$displayb("\nLRU bits input: ", LRU_bits);
+			$displayb("LRU bits actual: ", j);
+			$display("should be: ", check);
+			$display("block select: ", block_select);
 		end
 	end
 	$display("Finished.");
