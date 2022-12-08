c1b16711bf890d6dfd8061f040abbffb2b61388f
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-07 21:47:37 -0800

    Update eviction_lru.sv

--- eviction_lru.sv
+++ eviction_lru.sv
@@ -1,78 +1,60 @@
+import mypkg::*;
 
-module eviction_lru_tb;
-
-parameter integer associativity = 8;
-integer i;
-integer j;
-integer k;
-integer a = 0;
-
-integer debug = 0;
-//size of output and inputs
-reg [associativity - 2 : 0] returned;
-reg [associativity - 2 : 0] LRU_bits;
-reg [$clog2(associativity) - 1 : 0] block_select;
-reg [$clog2(associativity) - 1 : 0] check;
-reg [associativity - 2 : 0] temp = 0;
-
-int num_wrong = 0;
-
-eviction_lru #(.associativity(associativity)) test(block_select, LRU_bits);
+module eviction_LRU(block_select, LRU_bits, MESI);
+parameter integer a_size = 8;
 
 
-initial
-begin 
-	if($test$plusargs ("debug")) //checking if debug was called in the terminal
-		debug = 1;
-		
-	for(j = 0; j < 2 ** (associativity-1); j = j + 1) //next branch of LRU tree
+input [a_size - 2 : 0]LRU_bits; // size of LRU bits
+input [protocol - 1: 0] MESI[a_size];
+
+output reg [$clog2(a_size) - 1 : 0]block_select; //size of each block (based on log_2 calculation of associativity, log_2(8))
+
+int i = 0;
+int a = 0;
+int b = 0;
+
+int count = 0;
+
+
+
+
+
+always @(LRU_bits)
+begin
+	a = 0;
+	b = 0;
+	count = 2;
+
+	for(i = 0; i < $clog2(a_size); i = i + 1)
 	begin
-		LRU_bits = j;
-		a = 0;
-		for(k = 0; k < $clog2(associativity); k = k + 1) //final branch of LRU tree (the actual block)
-		begin
-			check[2-k] = LRU_bits[a]; //set the check[a] to the selected block value
-			if(LRU_bits[a] % 2 === 0) //if value is even
-			begin
-				check[2-k] = 1;
-				a = 2 * a + 2; // go right
-			end
-			else //value must be odd
-			begin
-				check[2-k] = 0;
-				a = 2 * a + 1; //go left
-			end
-		end
-		//$display("Before delay: ", LRU_bits);
-		#1
-		//$display("After delay: ", LRU_bits);
 
-		else if(debug === 1)
+		block_select[$clog2(a_size) - 1 - i] = LRU_bits[a];
+		if(LRU_bits[a] === 0) //if the selected block is even
 		begin
-			$displayb("\nLRU bits input: ", LRU_bits);
-			$displayb("LRU bits actual: ", j);
-			$display("should be: ", check);
-			$display("block select: ", block_select);
+			block_select[$clog2(a_size) - 1 - i] = 1;
+			a = 2 * a + 2; //go right
 		end
-		if(block_select ===  check || debug) //if the output value equals either the check value or debug
-		begin // display the evicted value
-			//$displayb("\nblock select: ", block_select);
-			//$displayb("LRU bits: ", j);
-			//$displayb("should be: ", check);
-			//$display("Evicted: ", block_select);
+	
+		else // selected block must be odd
+		begin
+			block_select[$clog2(a_size) - 1 - i] = 0;
+			a = 2 * a + 1; // go left
 		end
-		else //(returned !==  check || debug) //the output value does not equal the check value or debug
-		begin // display and compare the results to each other
-			num_wrong++;
-			$display("Incorrect");
-			$displayb("\nLRU bits input: ", LRU_bits);
-			$displayb("LRU bits actual: ", j);
-			$display("should be: ", check);
-			$display("block select: ", block_select);
+			
+		if(MESI[i] === 0 || MESI[i][0] === 1'bx)
+		begin
+			$display("MESI: ", MESI[i]);
+			block_select = i;
+			$display("block_select: ", block_select);
+			break;
 		end
+		
 	end
-	$display("Finished.");
-end
+
+
+end 
+
+
 
 
 endmodule
