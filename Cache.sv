85d9ef353f55057e58ebb3bf602d1cf0695b15e5
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-07 21:41:39 -0800

    Update Cache.sv

--- Cache.sv
+++ Cache.sv
@@ -22,14 +22,16 @@ input [i_size - 1 : 0] instruction;
 
 // inout [a_size + (protocol + i_size - c_size + a_size - d_size) * a_size - 2: 0] tag_array[2 ** (c_size - a_size)];
 int i=0;
+
+
 input common;
 
 output hit;
 output miss;
-output [1:0] bus_op_out; //bus operation commands 
-output [1:0] snoop_result; //update snoop_result 
+output [protocol - 1:0] bus_op_out; //bus operation commands 
+output reg [protocol - 1:0] snoop_result; //update snoop_result 
 output [1:0] L2_L1; // this is for communication between the L1 and L2
-int temp_index=36; //delete later
+
 
 cache_data tag_info[2 ** (c_size - $clog2(a_size)) - d_size];
 
@@ -40,31 +42,47 @@ reg [i_size - (c_size - d_size - $clog2(a_size)) - d_size - 1 : 0] tag; //tag bi
 wire [(c_size - $clog2(a_size)) - d_size - 1 : 0] index; // num of index bits
 wire [d_size - 1 : 0] byte_select; //num of byte_select bits
 reg [$clog2(a_size) - 1 : 0] block_select; // num of block_select (ways)
+reg [$clog2(a_size) - 1 : 0] evict_block;
 reg [a_size - 2 : 0] returned; // temp variable to hold data for tag_info.PLRU
+reg [protocol - 1:0] temp_snoop; //this is to hold the returned snoop result from get_snoop_result
+
 
 
 address_parse #(.i_size(i_size), .d_size(d_size), .c_size(c_size), .a_size(a_size)) a_parse (instruction, tag, index, byte_select);
-block_selector  #(.i_size(i_size), .d_size(d_size), .c_size(c_size), .a_size(a_size), .protocol(protocol)) selector (tag_info[index].tag, tag, block_select);
+hit_miss  #(.i_size(i_size), .d_size(d_size), .c_size(c_size), .a_size(a_size), .protocol(protocol)) hitmiss (tag, tag_info[index].tag, tag_info[index].protocol_bits, hit, miss, block_select);
 update_LRU #(.a_size(a_size)) uL (block_select, tag_info[index].PLRU, returned);
+eviction_LRU #(.a_size(a_size)) eL (evict_block, tag_info[index].PLRU, tag_info[index].protocol_bits);
+get_snoop_result #(.protocol(protocol)) snooping(byte_select[protocol - 1:0], temp_snoop);
 
-//assign tag_array[index][max_array : max_array - a_size + 1] = returned; 
-//assign tag_array[index][(protocol + i_size - c_size + a_size - d_size) * (block_select + 1) - protocol : (protocol + i_size - c_size + a_size - d_size) * block_select] = 
-
-always @*
+always @(command, instruction, common)
 begin
-	tag_info[index].PLRU = 5;
-	tag_info[index].tag[0] = 5;
-	tag = 5;
+
 	while(returned[0] === 1'bx)
 	begin
 		#1
-		$display("stuck: ", returned[0]);
-		$display(block_select);
-		$display(index);
+		//$display(miss);
+		//$display("stuck: ", returned[0]);
+		//$display(block_select);
+		//$display(index);
+	end
+
+	if(miss === 1 && (command === 0 || command === 1 || command === 2))
+	begin
+		//$display("evicted: ", evict_block);
+		tag_info[index].PLRU = returned;
+		#1
+		tag_info[index].tag[evict_block] = tag;
+		
 	end
 	
-	tag_info[index].PLRU = returned;
-	$display("we did it! ",tag_info[index].PLRU);
+	if(command === 3 || command === 4 || command === 5 || command === 6)
+	begin
+		snoop_result = temp_snoop;
+	end
+
+
+	$display("PLRU: ",tag_info[index].PLRU);
+	$display("Snoop result ", snoop_result);
 
 			
 
