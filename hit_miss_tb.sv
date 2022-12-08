19d957058b90d955a90c9f903bbac26234e6ea06
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-07 21:48:46 -0800

    Update hit_miss_tb.sv

--- hit_miss_tb.sv
+++ hit_miss_tb.sv
@@ -1,80 +1,54 @@
-module hit_miss_tb;
+module hit_miss(tag, /*index,*/ tag_array, MESI, hit, miss, block_select);
 
 //this uses powers of 2
-parameter integer i_size = 16;
-parameter integer c_size = 10;
-parameter integer d_size = 4;
-parameter integer protocol = 1;
+parameter integer i_size = 32;
+parameter integer c_size = 24;
+parameter integer d_size = 6;
+parameter integer protocol = 2;
 
 //this one is in bits
-parameter integer a_size = 4;
+parameter integer a_size = 8;
 
-reg [i_size - c_size + a_size - d_size - 1 : 0]tag;
-//reg [(c_size - a_size) - 1 : 0]index;
-reg [i_size - c_size + a_size - d_size - 1: 0] tag_array[a_size];
-reg MESI[a_size];
+input [i_size - (c_size - d_size - $clog2(a_size)) - d_size - 1 : 0]tag;
+//input [(c_size - a_size) - 1 : 0]index;
+input [i_size - (c_size - d_size - $clog2(a_size)) - d_size - 1 : 0] tag_array[a_size];
+input [protocol-1 : 0] MESI[a_size];
 
-integer debug = 0;
+output reg hit;
+output reg miss;
+output reg [$clog2(a_size) - 1 : 0] block_select;
 
-reg hit;
-reg miss; 
-
-hit_miss #(.i_size(i_size), .c_size(c_size), .d_size(d_size), .protocol(protocol), .a_size(a_size)) inst (tag, /*index,*/ tag_array, MESI, hit, miss);
-
-initial
+always @*
 begin
-	if($test$plusargs ("debug"))
-	begin
-		$display("Debug mode.\n");
-		debug = 1;
-	end
-	for(int i = 0; i < i_size - c_size + a_size - d_size; i = i + 1)
-	begin
-		tag = i;
-		for(int j1 = 0; j1 < i_size - c_size + a_size - d_size + 2; j1 = j1 + 1)
+	
+	//$display("hit_miss: ", tag);
+	//$display("Here I am.");
+	for(int i = 0; i < a_size; i = i + 1)
+	begin 
+		if(tag_array[i] === tag && MESI[i] !== 0)
 		begin
-			{tag_array[0], MESI[0]} = j1;
-			
-			for(int j2 = 0; j2 < i_size - c_size + a_size - d_size + 2; j2 = j2 + 1)
+			hit = 1;
+			miss = 0;
+			block_select = i;
+			break;
+		end
+
+		else
+		begin
+			hit = 0;
+			miss = 1;
+			//$display(" got a miss so far: ", MESI[i]);
+			if(MESI[i] === 0 || MESI[i][0] === 1'bx)
 			begin
-				{tag_array[1], MESI[1]} = j2;
-				for(int j3 = 0; j3 < i_size - c_size + a_size - d_size + 2; j3 = j3 + 1)
-				begin
-					{tag_array[2], MESI[2]} = j3;
-					for(int j3 = 0; j3 < i_size - c_size + a_size - d_size + 2; j3 = j3 + 1)
-					begin
-						{tag_array[3], MESI[3]} = j3;
-						#5
-						if(((MESI[0] !== 1 || MESI[1] !== 1 ) || tag_array[0] !== tag_array[1]) && ((MESI[0] !== 1 || MESI[2] !== 1 ) || tag_array[0] !== tag_array[2]) && ((MESI[0] !== 1 || MESI[3] !== 1 ) || tag_array[0] !== tag_array[3]) && ((MESI[1] !== 1 || MESI[2] !== 1 ) || tag_array[1] !== tag_array[2]) && ((MESI[1] !== 1 || MESI[3] !== 1 ) || tag_array[1] !== tag_array[3]) && ((MESI[2] !== 1 || MESI[3] !== 1 ) || tag_array[2] !== tag_array[3]))
-						begin
-							if(({tag,1'b1} === {tag_array[0],MESI[0]} || {tag,1'b1} === {tag_array[1],MESI[1]} || {tag,1'b1} === {tag_array[2],MESI[2]} || {tag,1'b1} === {tag_array[3],MESI[3]}) && hit !== 1)
-							begin
-								$display("Incorrect.");
-								$display("Sent: ", tag_array[0], " ", MESI[0], ", ", tag_array[1]," ", MESI[1],  ", ", tag_array[2]," ", MESI[2],  ", ", tag_array[3]," ", MESI[3]);
-								$display("no hit for tag: ", tag, "\n");
-								$display("Hit: ", hit, " Miss: ", miss);
-							end
-							
-							else if(({tag,1'b1} !== {tag_array[0],MESI[0]} && {tag,1'b1} !== {tag_array[1],MESI[1]} && {tag,1'b1} !== {tag_array[2],MESI[2]} && {tag,1'b1} !== {tag_array[3],MESI[3]}) && miss !== 1)
-							begin
-								$display("Incorrect.");
-								$display("Sent: ", tag_array[0], " ", MESI[0], ", ", tag_array[1]," ", MESI[1],  ", ", tag_array[2]," ", MESI[2],  ", ", tag_array[3]," ", MESI[3]);
-								$display("no miss for tag: ", tag, "\n");
-								$display("Hit: ", hit, " Miss: ", miss);
-							end
-							
-							else if(debug === 1)
-							begin
-								$display("Correct.\n");
-							end
-						end
-					end
-				end
+				//$display("Here to update block_select");
+				block_select = i;
+				break;
 			end
 		end
+		//$display("Hit: ", hit, " Miss: ", miss, " \n\n\n");
 	end
-	$display("FINISHED.");
+
 end
+
 endmodule
-							
-						
+	
