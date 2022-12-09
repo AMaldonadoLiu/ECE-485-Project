b1c16813bf2836ae810a7e80c8fd421ee4591c44
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-09 08:06:51 -0800

    Update address_parse.sv

--- address_parse.sv
+++ address_parse.sv
@@ -1,3 +1,5 @@
+import mypkg::*;
+
 module address_parse(address, tag, index, byte_select);
 //parameters to be changed depending on the architecture (in bits used to represent them)
 parameter integer i_size = 64;
@@ -9,13 +11,20 @@ parameter integer a_size = 8;
 input [i_size - 1: 0]address;
 
 //the size of the output section arrays
-output [d_size - 1 : 0]byte_select;
-output [(c_size - $clog2(a_size) - d_size) - 1 : 0]index;
-output [i_size - (c_size - d_size - $clog2(a_size)) - d_size - 1 : 0]tag;
+output reg [d_size - 1 : 0]byte_select;
+output reg [(c_size - $clog2(a_size) - d_size) - 1 : 0]index;
+output reg [tag_bits - 1 : 0]tag;
 
 //assign different sections of the data to each output
-assign byte_select = address[d_size - 1 : 0];
-assign index = address[(c_size - $clog2(a_size) - d_size ) + d_size - 1 : d_size];
-assign tag = address[i_size - 1 : (c_size - $clog2(a_size)) + d_size];
+//assign byte_select = address[d_size - 1 : 0];
+//assign index = address[(c_size - $clog2(a_size) - d_size ) + d_size - 1 : d_size];
+//assign tag = address[i_size - 1 : (c_size - $clog2(a_size)) + d_size];
+
+always @*
+begin
+	byte_select = address[d_size - 1 : 0];
+	index = address[(c_size - $clog2(a_size) - d_size ) + d_size - 1 : d_size];
+	tag = address[i_size - 1 : index_bits + d_size]; 
+end
 
 endmodule
