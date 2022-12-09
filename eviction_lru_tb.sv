0155ecef66a8cc70ebe7d85fe3a185dcc4fcf10d
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-09 08:09:02 -0800

    Update eviction_lru_tb.sv

--- eviction_lru_tb.sv
+++ eviction_lru_tb.sv
@@ -16,17 +16,26 @@ reg [$clog2(associativity) - 1 : 0] block_select;
 reg [$clog2(associativity) - 1 : 0] check;
 reg [associativity - 2 : 0] temp = 0;
 reg [protocol - 1 : 0] protocol_bits;
-
+reg [1:0] MESI[associativity];
 int num_wrong = 0;
 
-eviction_LRU #(.a_size(associativity)) test(block_select, LRU_bits, 1);
+eviction_LRU #(.a_size(associativity)) test(block_select, LRU_bits, MESI);
 
 
 initial
 begin 
 	if($test$plusargs ("debug")) //checking if debug was called in the terminal
 		debug = 1;
-		
+	
+
+for(int m = 0; m < associativity; m = m + 1)
+begin
+
+for(int i = 0; i < 2 ** protocol; i = i + 1)
+begin
+	
+	MESI[m] = i;
+	
 	for(j = 0; j < 2 ** (associativity-1); j = j + 1) //next branch of LRU tree
 	begin
 		LRU_bits = j;
@@ -56,7 +65,7 @@ begin
 			$display("should be: ", check);
 			$display("block select: ", block_select);
 		end
-		else if(block_select ===  check || debug) //if the output value equals either the check value or debug
+		else if((block_select ===  check && MESI[block_select] != 0) || debug) //if the output value equals either the check value or debug
 		begin // display the evicted value
 			//$displayb("\nblock select: ", block_select);
 			//$displayb("LRU bits: ", j);
@@ -65,16 +74,20 @@ begin
 		end
 		else //(returned !==  check || debug) //the output value does not equal the check value or debug
 		begin // display and compare the results to each other
-			num_wrong++;
-			$display("Incorrect");
-			$displayb("\nLRU bits input: ", LRU_bits);
-			$displayb("LRU bits actual: ", j);
-			$display("should be: ", check);
-			$display("block select: ", block_select);
+			if(MESI[block_select] != 0)
+			begin
+				num_wrong++;
+				$display("Incorrect");
+				$displayb("\nLRU bits input: ", LRU_bits);
+				$displayb("LRU bits actual: ", j);
+				$display("should be: ", check);
+				$display("block select: ", block_select);
+			end
 		end
 	end
 	$display("Finished.");
 end
-
+end
+end
 
 endmodule
