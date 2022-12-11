edea49e8a97d20c125be3daa6025a2035e0ce98d
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-10 19:07:06 -0800

    Update MESI_protocol.sv

--- MESI_protocol.sv
+++ MESI_protocol.sv
@@ -1,17 +1,21 @@
 import mypkg::*;
 
-module MESI( cmd,snoopResult,MESI_bits,miss,snoopResultout,busop_out,L2_L1,mesiReturned);
+module MESI( cmd,snoopResult,MESI_bits,miss,snoopResultout,busop_out,L2_L1,mesiReturned, silent);
+
+parameter integer busopsize = 3;
+
 
 input reg [3:0] cmd;
 input reg [1:0] snoopResult;
 //input reg block_select;
 input reg [1:0] MESI_bits;
 input reg miss;
+input reg silent;
 
 //input reg way;
 //output reg snoopResultout;
-output reg [1:0] busop_out;
-output reg [1:0] L2_L1;
+output reg [1:0] busop_out[busopsize];
+output reg [1:0] L2_L1[busopsize];
 output reg [1:0] mesiReturned;
 output reg [1:0] snoopResultout;
 //input reg hit=1;
@@ -20,12 +24,21 @@ enum{READ=0,WRITE,L1_READ,SNOOP_INVAL,SNOOPED_RD,SNOOP_WR,
 
 always @ (*)
 begin
-	
+	//$display("snoop = ", snoopResult);
+	//$display("MESI PROTOCOL");
 	if(MESI_bits === 2'bxx)
 	begin
 		MESI_bits = 0;
 	end
-	busop_out = 2'bxx;
+	for(int i = 0; i < busopsize; i = i + 1)
+	begin
+		busop_out[i] = 2'bxx;
+	end
+	for(int n = 0; n < busopsize; n = n + 1)
+	begin
+		L2_L1[n] = 2'bxx;
+	end
+
 	snoopResultout = 2'bxx;
 	case(MESI_bits)
 
@@ -36,44 +49,49 @@ begin
 
 				if(miss === 1 && (snoopResult === HIT || snoopResult === HITM))
 				begin
-					busop_out = BWRITE;
-					busop_out = BREAD;
+					busop_out[0] = BWRITE;
+
+					busop_out[1] = BREAD;
+
 					mesiReturned = S;
 				end
 
 				else if(miss === 1 && (snoopResult === NOHIT || snoopResult === NOHIT-1))
 				begin
-					busop_out = BWRITE;
-					busop_out = BREAD;
+					busop_out[0] = BWRITE;
+
+					busop_out[1] = BREAD;
+
 					mesiReturned = E;
 				end
 				
 				else
 					mesiReturned = M;
-				L2_L1 = SENDLINE;
-				
+				L2_L1[0] = SENDLINE;
+
 			end
 
 			else if(cmd === 1)
 			begin
 
-				if(snoopResult === HIT || snoopResult === HITM)
+				if(miss === 1)
 				begin
-					L2_L1 = EVICTLINE;
-					busop_out = BRWIM;
+					L2_L1[0] = EVICTLINE;
+					busop_out[0] = BRWIM;
+					L2_L1[1] = SENDLINE;
+					L2_L1[2] = GETLINE;
 
 				end
 
-				else
+				else if(miss === 0)
 				begin
-					L2_L1 = SENDLINE;
-					busop_out = BINVAL;
+					L2_L1[0] = SENDLINE;
+					busop_out[0] = BINVAL;
+
 				end
 				
 				mesiReturned = M;
 
-				if(miss === 1)
-					L2_L1 = GETLINE;
 
 				//L2_L1 = GETLINE;
 				//since it is write through only once from L1, this means that since we hit and are modified, we don't GETLINE
@@ -82,23 +100,26 @@ begin
 			else if(cmd === 4 && miss != 1)
 			begin
 				snoopResultout = HITM;
-				L2_L1 = GETLINE;
-				busop_out = BWRITE;
+				L2_L1[0] = GETLINE;
+				busop_out[0] = BWRITE;
+
 				mesiReturned = S;
 			end
 
 			else if(cmd === 6 && miss != 1)
 			begin
 				snoopResultout = HITM;
-				L2_L1 = EVICTLINE;
-				busop_out = BWRITE;
+				L2_L1[0] = EVICTLINE;
+				busop_out[0] = BWRITE;
+
 				mesiReturned = I;
 			end	
 	
 			else if(cmd === 8)
 			begin
-				L2_L1 = EVICTLINE;
-				busop_out = BWRITE;
+				L2_L1[0] = EVICTLINE;
+				busop_out[0] = BWRITE;
+
 				mesiReturned = I;
 			end
 
@@ -115,18 +136,20 @@ begin
 			begin
 				if(miss === 1 && (snoopResult === HIT || snoopResult === HITM))
 				begin
-					L2_L1 = INVALLINE;
-					busop_out = BREAD;
+					L2_L1[0] = INVALLINE;
+					busop_out[0] = BREAD;
+
 
 					mesiReturned = S;
 				end
 				else if(miss === 1 && (snoopResult === NOHIT || snoopResult === NOHIT+1))
 				begin
-					L2_L1 = INVALLINE;
-					busop_out = BREAD;
+					L2_L1[0] = INVALLINE;
+					busop_out[0] = BREAD;
+
 					mesiReturned = E;
 				end
-				L2_L1 = SENDLINE;
+				L2_L1[1] = SENDLINE;
 
 			end
 
@@ -135,22 +158,24 @@ begin
 			begin
 				if(miss === 1 && (snoopResult === HIT || snoopResult === HITM))
 				begin
-					L2_L1 = INVALLINE;
-					busop_out = BRWIM;
+					L2_L1[0] = INVALLINE;
+					busop_out[0] = BRWIM;
+
 					
 				end
 
 
 				else if(miss === 1 && (snoopResult === NOHIT || snoopResult === NOHIT+1))
 				begin
-					L2_L1 = INVALLINE;
-					busop_out = BRWIM;
+					L2_L1[0] = INVALLINE;
+					busop_out[0] = BRWIM;
+
 					//L2_L1 = SENDLINE;
 				end
 
-				L2_L1 = SENDLINE;
+				L2_L1[1] = SENDLINE;
 				mesiReturned = M;
-				L2_L1 = GETLINE;
+				L2_L1[2] = GETLINE;
 				
 			end
 
@@ -165,7 +190,7 @@ begin
 
 			else if((cmd === 6 && miss === 0) || cmd === 8)
 			begin
-				L2_L1 = INVALLINE;
+				L2_L1[0] = INVALLINE;
 				mesiReturned = I;
 			end
 
@@ -181,23 +206,25 @@ begin
 			begin
 				if(miss === 1 && (snoopResult ===  HIT || snoopResult === HITM))
 				begin
-					L2_L1 = EVICTLINE;
-					busop_out = BREAD;
+					L2_L1[0] = EVICTLINE;
+					busop_out[0] = BREAD;
+
 					mesiReturned = S;
-					L2_L1 = SENDLINE;
+					L2_L1[1] = SENDLINE;
 				end
 				else if( miss === 1 && (snoopResult === NOHIT || snoopResult === NOHIT + 1))
 				begin
-					L2_L1 = EVICTLINE;
-					busop_out = BREAD;
+					L2_L1[0] = EVICTLINE;
+					busop_out[0] = BREAD;
+
 					mesiReturned = E;
-					L2_L1 = SENDLINE;
+					L2_L1[1] = SENDLINE;
 				end
 
 				else
 				begin
 					mesiReturned = S;
-					L2_L1 = SENDLINE;
+					L2_L1[0] = SENDLINE;
 				end
 				
 
@@ -208,22 +235,23 @@ begin
 			begin
 				if(miss === 1)
 				begin
-					L2_L1 = EVICTLINE;
-					busop_out = BRWIM;
-					L2_L1 = SENDLINE;
+					L2_L1[0] = EVICTLINE;
+					busop_out[0] = BRWIM;
+
+					L2_L1[1] = SENDLINE;
 				end
 				
 				else
-					busop_out = BINVAL;
+					busop_out[0] = BINVAL;
 				mesiReturned = M;
 
-				L2_L1 = GETLINE;
+				L2_L1[2] = GETLINE;
 				
 			end
 
 			else if(cmd === 3 && miss === 0)
 			begin
-				L2_L1 = INVALLINE;
+				L2_L1[0] = INVALLINE;
 				mesiReturned = I;
 				
 			end
@@ -268,18 +296,20 @@ begin
 			begin
 				if(snoopResult == HIT|| snoopResult == HITM)
 				begin
-					busop_out = BREAD;
+					busop_out[0] = BREAD;
+
 					mesiReturned = S;
 				end
 
 				else if(snoopResult == NOHIT)
 				begin
 
-					busop_out = BREAD;
+					busop_out[0] = BREAD;
+
 					mesiReturned = E;
 
 				end
-				L2_L1 = SENDLINE;
+				L2_L1[0] = SENDLINE;
 			end
 
 
@@ -287,11 +317,12 @@ begin
 			begin
 				if(snoopResult === HIT || snoopResult === HITM)
 				begin 
-					busop_out = BRWIM;
+					busop_out[0] = BRWIM;
+
 				end
-				L2_L1 = SENDLINE;
+				L2_L1[0] = SENDLINE;
 				mesiReturned = M;
-				L2_L1 = GETLINE;
+				L2_L1[1] = GETLINE;
 			end	
 
 
@@ -302,10 +333,7 @@ begin
 		end
 
 	endcase
-	if(busop_out === 2'bxx)
-		busop_out = 2'bzz;
-	if(snoopResultout === 2'bxx)
-		snoopResultout = 2'bzz;
+
 	//$display("finished");
 end
 
