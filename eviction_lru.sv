1af9ed76b19251ac7a0c5c5cec552d874154f795
AMaldonadoLiu <92129710+AMaldonadoLiu@users.noreply.github.com>
2022-12-10 19:06:18 -0800

    Update eviction_lru.sv

--- eviction_lru.sv
+++ eviction_lru.sv
@@ -1,11 +1,12 @@
 import mypkg::*;
 
-module eviction_LRU(block_select, LRU_bits, MESI);
+module eviction_LRU(block_select, LRU_bits, MESI, miss);
 parameter integer a_size = 8;
 
 
 input [a_size - 2 : 0]LRU_bits; // size of LRU bits
 input [protocol - 1: 0] MESI[a_size];
+input miss;
 
 output reg [$clog2(a_size) - 1 : 0]block_select; //size of each block (based on log_2 calculation of associativity, log_2(8))
 
@@ -21,37 +22,42 @@ int count = 0;
 
 always @(LRU_bits)
 begin
+	//$display("EVICTION LRU");
 	a = 0;
 	b = 0;
 	count = 2;
-
-	for(i = 0; i < $clog2(a_size); i = i + 1)
+	block_select[0] = 1'bz;
+	if(miss == 1)
 	begin
 
-		block_select[$clog2(a_size) - 1 - i] = LRU_bits[a];
-		if(LRU_bits[a] === 0) //if the selected block is even
+		for(i = 0; i < $clog2(a_size); i = i + 1)
 		begin
-			block_select[$clog2(a_size) - 1 - i] = 1;
-			a = 2 * a + 2; //go right
-		end
+			if(MESI[i] === 0 || MESI[i][0] === 1'bx)
+			begin
+				//$display("MESI: ", MESI[i]);
+				block_select[0] = 1'bz;
+				//$display("block_select: ", block_select);
+				break;
+			end
+
+			block_select[$clog2(a_size) - 1 - i] = ~LRU_bits[a];
+			if(LRU_bits[a] === 0) //if the selected block is even
+			begin
+				block_select[$clog2(a_size) - 1 - i] = 1;
+				a = 2 * a + 2; //go right
+			end
 	
-		else // selected block must be odd
-		begin
-			block_select[$clog2(a_size) - 1 - i] = 0;
-			a = 2 * a + 1; // go left
-		end
+			else // selected block must be odd
+			begin
+				block_select[$clog2(a_size) - 1 - i] = 0;
+				a = 2 * a + 1; // go left
+			end
+				
 			
-		if(MESI[i] === 0 || MESI[i][0] === 1'bx)
-		begin
-			//$display("MESI: ", MESI[i]);
-			block_select = i;
-			//$display("block_select: ", block_select);
-			break;
-		end
 		
+		end
+
 	end
-
-
 end 
 
 
